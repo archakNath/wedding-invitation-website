3b5404aeeb8494b5d88594d2b0c0774a45c3a4c1
Archak Nath <112867859+archakNath@users.noreply.github.com>
2022-10-28 07:24:32 +0530

    added all files to repo

---
+++ main.js
@@ -0,0 +1,48 @@
+const parallax = document.getElementById("home-img-lg");
+const parallax1 = document.getElementById("parallax1");
+const parallax2 = document.getElementById("parallax2");
+
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionX = offset*(-0.3)-100 + "px";
+})
+
+
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    offset-=3100;
+    parallax1.style.backgroundPositionY = offset*(0.1) + "px";
+})
+
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    offset-=4800;
+    parallax2.style.backgroundPositionY = offset*(-0.1) + "px";
+})
+
+function myFunction() {
+    document.getElementById("check").checked = false;
+  }
+
+
+  
+function reveal() {
+var reveals = document.querySelectorAll(".reveal");
+  
+for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+  
+window.addEventListener("scroll", reveal);
